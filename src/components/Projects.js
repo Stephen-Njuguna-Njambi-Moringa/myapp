5992075bd8ca0ba7e758142e446a8ad789d0b836
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-10-07 17:44:19 +0300

    modified project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,46 +1,42 @@
-// import { useState, useEffect } from "react";
-// import React from 'react';
 
-// function Projects() {
-//     // const [projects, setProjects] = useState([]);
-//     // const fetchProjects = async () => {
-//     //     let res = await fetch('http://localhost:3000/projects');
-//     //     let data = await res.json();
-//     //     console.log(data);
-//     //     setProjects(data)
-//     // };
-//     // useState(() => {
-//     //     fetchProjects();
-//     // }, []);
+import { useState} from "react";
+import React from 'react';
 
-//     const [projects, setProjects] = useState([]);
-//         useEffect(() => {
-//             const requestOptions = {
-//                 method: 'POST',
-//                 headers: {'content-type' : 'application/json'},
-//                 body: JSON.stringify({title: 'React Hooks POST Request Example'})
-//             };
-//             fetch('http://localhost:3000/projects', requestOptions)
-//                 .then(response => response.json())
-//                 .then(data => setProjects(data.id))
-//         })
+function Projects() {
+    const [projects, setProjects] = useState([]);
+  const fetchProjects = async () => {
+    let res = await fetch('http://localhost:3000/projects');
+    let data = await res.json();
+    console.log(data);
+    setProjects(data)
+  };
 
-//     return (
-//         <>
-//             <div className="card mb-5">
-//                 <div className="card-header">
-//                 {projects.name}
-//                 {projects.id}
-//                 </div>
-//                 <div className="card-body">
-//                     <blockquote className="blockquote mb-0">
-//                     <p>A well-known quote, contained in a blockquote element.</p>
-//                     <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-//                     </blockquote>
-//                 </div>
-//             </div> 
-//         </>
-//     );
-// }
+  useState(() => {
+    fetchProjects();
+  }, []);
+ 
 
-// export default Projects;
+    return (
+        <div>
+            {projects.map((project) =>{
+                return(
+                    <div className="card mb-5">
+                    <div className="card-header">
+                        <h2>{project.project_name}</h2>
+                    </div>
+                    <div className="card-body">
+                        <blockquote className="blockquote mb-0">
+                            <p>{project.project_desc}</p>
+                            <footer className="blockquote-footer">{project.owner}</footer>
+                        </blockquote>
+                    </div>
+                </div> 
+                )
+            })}
+           
+        </div>
+    );
+}
+
+export default Projects;
+
