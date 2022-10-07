de1de32fb522926f8d2700a4d9f0be9531eac98f
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-10-07 17:46:34 +0300

    modified Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,25 @@
-// import React from 'react';
+import React from 'react';
 
-// function Hero(props) {
-//     return (
-//         <div>
-//             <div className="container col-xxl-8 px-4 py-5">
-//                 <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-//                 <div className="col-10 col-sm-8 col-lg-6">
-//                     <img src="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
-//                 </div>
-//                 <div className="col-lg-6">
-//                     <h1 className="display-5 fw-bold lh-1 mb-3">Software Engineering Social Platform</h1>
-//                     <p className="lead">We are a global Community programmers who share common dreams and great Ideas on Development.</p>
-//                     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-//                     <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Sign up</button>
-//                     <button type="button" className="btn btn-outline-secondary btn-lg px-4">Log in</button>
-//                     </div>
-//                 </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+function Hero(props) {
+    return (
+        <div>
+            <div className="container col-xxl-8 px-4 py-5">
+                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div className="col-10 col-sm-8 col-lg-6">
+                    <img src="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+                </div>
+                <div className="col-lg-6">
+                    <h1 className="display-5 fw-bold lh-1 mb-3">Software Engineering Social Platform</h1>
+                    <p className="lead">We are a global Community programmers who share common dreams and great Ideas on Development.</p>
+                    <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Sign up</button>
+                    <button type="button" className="btn btn-outline-secondary btn-lg px-4">Log in</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-// export default Hero;
+export default Hero;
